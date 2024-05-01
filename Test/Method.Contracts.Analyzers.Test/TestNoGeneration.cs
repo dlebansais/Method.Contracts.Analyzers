555b1bf3734f72f316fc466bc70003a1d2189a76
David Le Bansais <dlebansais@gmail.com>
2024-05-01 14:37:24 +0200

    Many updates.

---
+++ Test/Method.Contracts.Analyzers.Test/TestNoGeneration.cs
@@ -0,0 +1,24 @@
+﻿namespace Contracts.Analyzers.Test;
+
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+[TestFixture]
+public class TestNoGeneration
+{
+    [Test]
+    public async Task TestSuccess()
+    {
+        // The source code to test
+        const string Source = @"
+using Contracts.TestSuite;
+
+public class SimpleTest
+{
+}
+";
+
+        // Pass the source code to our helper and snapshot test the output
+        await TestHelper.Verify(Source).ConfigureAwait(false);
+    }
+}
---
+++ Test/Method.Contracts.Analyzers.Test/Tools/TestHelper.cs
@@ -0,0 +1,43 @@
+﻿namespace Contracts.Analyzers.Test;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Contracts.Analyzers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using VerifyNUnit;
+
+public static class TestHelper
+{
+    public static Task Verify(string source)
+    {
+        // Parse the provided string into a C# syntax tree
+        SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(source);
+
+        // Create references for assemblies we require
+        // We could add multiple references if required
+        IEnumerable<PortableExecutableReference> references =
+        [
+            MetadataReference.CreateFromFile(typeof(object).Assembly.Location)
+        ];
+
+        // Create a Roslyn compilation for the syntax tree.
+        CSharpCompilation compilation = CSharpCompilation.Create(
+            assemblyName: "Tests",
+            syntaxTrees: [syntaxTree],
+            references: references);
+
+
+        // Create an instance of our EnumGenerator incremental source generator
+        var generator = new ContractGenerator();
+
+        // The GeneratorDriver is used to run our generator against a compilation
+        GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+
+        // Run the source generator!
+        driver = driver.RunGenerators(compilation);
+
+        // Use verify to snapshot test the source generator output!
+        return Verifier.Verify(driver);
+    }
+}
