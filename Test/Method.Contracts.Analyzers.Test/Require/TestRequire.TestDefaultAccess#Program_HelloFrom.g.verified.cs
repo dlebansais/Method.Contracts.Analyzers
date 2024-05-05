d9253016641d9514056d2694f7b2fa7a93b38ad9
David Le Bansais <dlebansais@gmail.com>
2024-05-05 19:29:36 +0200

    Fixed tests.

--- Test/Method.Contracts.Analyzers.Test/Require/TestRequire.TestDefaultAccess#Program_HelloFrom.g.verified.cs
+++ Test/Method.Contracts.Analyzers.Test/Require/TestRequire.TestDefaultAccess#Program_HelloFrom.g.verified.cs
@@ -6,7 +6,7 @@ using System.CodeDom.Compiler;
 
 partial class Program
 {
-    [GeneratedCodeAttribute("Method.Contracts.Analyzers","1.3.0.1")]
+    [GeneratedCodeAttribute("Method.Contracts.Analyzers","1.3.1.1")]
     public static void HelloFrom(string text, out string textPlus)
     {
         Contract.Require(text.Length > 0);
