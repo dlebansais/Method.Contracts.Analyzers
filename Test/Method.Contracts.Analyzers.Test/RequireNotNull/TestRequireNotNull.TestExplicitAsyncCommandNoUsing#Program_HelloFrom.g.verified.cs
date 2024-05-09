314679d861196dc74cdff78d2702260828bf9800
David Le Bansais <dlebansais@gmail.com>
2024-05-09 18:45:34 +0200

    Added a missing test.

---
+++ Test/Method.Contracts.Analyzers.Test/RequireNotNull/TestRequireNotNull.TestExplicitAsyncCommandNoUsing#Program_HelloFrom.g.verified.cs
@@ -0,0 +1,20 @@
+﻿//HintName: Program_HelloFrom.g.cs
+#nullable enable
+
+namespace Contracts.TestSuite;
+
+using System;
+using System.CodeDom.Compiler;
+using System.Threading.Tasks;
+using Contracts;
+
+partial class Program
+{
+    [GeneratedCodeAttribute("Method.Contracts.Analyzers","1.3.5.7")]
+    public static async Task HelloFrom(string text, out string textPlus)
+    {
+        Contract.RequireNotNull(text, out string Text);
+
+        await HelloFromVerified(Text, out textPlus);
+    }
+}
