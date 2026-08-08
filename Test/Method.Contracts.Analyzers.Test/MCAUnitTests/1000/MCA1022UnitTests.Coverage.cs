a9a61ae4daef3576a9d943f465df25b81c94be4b
David Le Bansais <dlebansais@gmail.com>
2026-08-08 21:01:08 +0200

    Restored missing tests.

---
+++ Test/Method.Contracts.Analyzers.Test/MCAUnitTests/1000/MCA1022UnitTests.Coverage.cs
@@ -0,0 +1,41 @@
+﻿namespace Contracts.Analyzers.Test;
+
+extern alias Analyzers;
+
+using System.Threading.Tasks;
+using NUnit.Framework;
+using VerifyCS = CSharpAnalyzerVerifier<Analyzers.Contracts.Analyzers.MCA1022UnverifiedMethodIsUsingSuffix>;
+
+[TestFixture]
+internal partial class MCA1022UnitTests
+{
+    [Test]
+    public async Task CoverageDirective_NoDiagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(@"
+#define COVERAGE_A25BDFABDDF8402785EB75AD812DA952
+" + Prologs.Default, @"
+internal partial class Program
+{
+    private static void HelloFromFooVerified(string text, out string textPlus)
+    {
+        textPlus = text + ""!"";
+    }
+}
+", Microsoft.CodeAnalysis.CSharp.LanguageVersion.CSharp7).ConfigureAwait(false);
+    }
+
+    [Test]
+    public async Task OldLanguageVersion_NoDiagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(Prologs.Default, @"
+internal partial class Program
+{
+    private static void HelloFromFooVerified(string text, out string textPlus)
+    {
+        textPlus = text + ""!"";
+    }
+}
+", Microsoft.CodeAnalysis.CSharp.LanguageVersion.CSharp6).ConfigureAwait(false);
+    }
+}
---
+++ Test/Method.Contracts.Analyzers.Test/MCAUnitTests/1000/MCA1022UnitTests.cs
@@ -0,0 +1,67 @@
+﻿namespace Contracts.Analyzers.Test;
+
+extern alias Analyzers;
+
+using System.Threading.Tasks;
+using NUnit.Framework;
+using VerifyCS = CSharpAnalyzerVerifier<Analyzers.Contracts.Analyzers.MCA1022UnverifiedMethodIsUsingSuffix>;
+
+[TestFixture]
+internal partial class MCA1022UnitTests
+{
+    [Test]
+    public async Task FooPrefix_Diagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(@"
+internal partial class Program
+{
+    private static void [|HelloFromFooVerified|](string text, out string textPlus)
+    {
+        textPlus = text + ""!"";
+    }
+}
+").ConfigureAwait(false);
+    }
+
+    [Test]
+    public async Task NoVerifiedPrefix_NoDiagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(Prologs.Nullable, @"
+internal partial class Program
+{
+    private static void HelloFrom(string text, out string textPlus)
+    {
+        textPlus = text + ""!"";
+    }
+}
+").ConfigureAwait(false);
+    }
+
+    [Test]
+    public async Task FooPrefixNullable_Diagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(Prologs.Nullable, @"
+internal partial class Program
+{
+    private static void [|HelloFromFooVerified|](string text, out string textPlus)
+    {
+        textPlus = text + ""!"";
+    }
+}
+").ConfigureAwait(false);
+    }
+
+    [Test]
+    public async Task OnlyPrefix_Diagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(@"
+internal partial class Program
+{
+    private static void [|Verified|](string text, out string textPlus)
+    {
+        textPlus = text + ""!"";
+    }
+}
+").ConfigureAwait(false);
+    }
+}
